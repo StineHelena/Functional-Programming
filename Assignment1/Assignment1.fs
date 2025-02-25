2e762f629319af0f7dbed37bc964eb13c9f3a738
sths <stinehelena09@gmail.com>
2025-02-25 09:24:54 +0100

    Added some notes for the assignments

--- Assignment1/Assignment1.fs
+++ Assignment1/Assignment1.fs
@@ -2,14 +2,25 @@
 
     open System
     
+    //Write a function sqr : int -> int that given an integer x returns x squared
     let sqr x = x * x
-
+    
+    //Write a function pow : float -> float -> float that given two floating point numbers x and n returns x to the power of n
     let pow x n = Math.Pow(x, n)
     
+    //Write a recursive function sum : int -> int such that given an integer n such that n 0 returns the sum of all integers from 0 to n inclusive.
     let rec sum = function
         |   0 -> 0
-        |   n -> n + sum(n-1)
+        |   n -> n + sum(n-1)  
+    //You can write both sum(n-1) like above but it will also work with sum n-1.
+    //Just remember not to have spaces between the params or it will not work
+    //So n-1 will but n - 1 will not work
+    let rec sumAlt = function
+        |   0 -> 0
+        |   n -> n + sum n-1  
+    
 
+    //Fibonacci with recursion. Write a function fib : int -> int
     let rec fib = function
         | 0 -> 0
         | 1 -> 1
