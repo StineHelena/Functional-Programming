ef783a8d6c19e2429a2a7a67afe96ec75be1d0e4
sths <stinehelena09@gmail.com>
2025-03-04 16:23:48 +0100

    Made some of the 4 assignment

--- Assignment4/Eval.fs
+++ Assignment4/Eval.fs
@@ -1,9 +1,57 @@
 module Interpreter.Eval
 
+    open Interpreter.Language
     open Result
     open Language
     open State
     
-    let arithEval _ = failwith "not implemented"    
-    let boolEval _ = failwith "not implemented"
-    let stmntEval _ = failwith "not implemented"
+    let rec arithEval (a : aexpr) (st : state) : int option =
+        match a with
+        | Num x -> Some x
+        | Var v -> getVar v st
+        | Add(b, c) ->
+            let b' = arithEval b st
+            let c' = arithEval c st
+            if b' = None || c' = None then
+                None
+                else
+                Some (b'.Value + c'.Value)
+        | Mul(b, c) ->
+            let b' = arithEval b st
+            let c' = arithEval c st
+            if b' = None || c' = None then
+                None
+                else
+                Some (b'.Value * c'.Value)
+        | Div(b, c) ->
+            let b' = arithEval b st
+            let c' = arithEval c st
+            if b' = None || c' = None || c'.Value = 0 then
+                None
+                else
+                    Some (b'.Value / c'.Value)
+        | Mod(b, c) ->
+            let b' = arithEval b st
+            let c' = arithEval c st
+            if b' = None || c' = None || c'.Value = 0 then
+                None
+                else
+                    Some (b'.Value % c'.Value)
+        | _ -> None
+
+    let arithEval2 (a : aexpr) (st : state) : int option =
+        match a with
+        | Num x -> Some x
+        | Var v -> getVar v st
+        | Add(b, c) ->
+            let b' = arithEval b st
+            let c' = arithEval c st
+            Some 0
+            //Option.bind (fun ry -> ry) y
+            //Option.bind (fun rx -> Option.bind (fun ry -> Some (rx + ry))y )x
+        | Mul(b, c) ->
+            let b' = arithEval b st
+            let c' = arithEval c st
+            Some (b'.Value * c'.Value)
+    let boolEval (b : bexpr) (st : state) : bool option = failwith "not implemented"
+    let stmntEval (s : stmnt) (st : state) : state option = failwith "not implemented"
--- Assignment4/State.fs
+++ Assignment4/State.fs
@@ -16,11 +16,28 @@ module Interpreter.State
     
     let mkState () = {map = Map.empty }
     let random _ = failwith "not implemented"
-    let declare _ = failwith "not implemented"
-            
+    let declare x (st : state) =
+        match x, st with
+        | x, st when (validVariableName(x))
+                     && (reservedVariableName(x) = false)
+                     && (Map.containsKey x st.map = false) ->
+            let st' = {map = Map.add x 0 st.map}
+            Some st'
+        | _ -> None
     
-    let getVar _ = failwith "not implemented"
-    let setVar _ = failwith "not implemented"
+    let getVar (x : string) (st : state) : int option =
+        match x, st with
+        | x, st when (Map.containsKey x st.map = true) ->
+            let variableV' = Map.find x st.map
+            Some variableV'
+        | _ -> None
+        
+    let setVar (x : string) (v : int) (st : state) : state option =
+        match x, v, st with
+        | x, v, st when (Map.containsKey x st.map = true) ->
+            let updateSt' = {map = Map.add x v st.map}
+            Some updateSt'
+        | _ -> None
     
     let push _ = failwith "not implemented"
     let pop _ = failwith "not implemented"     
